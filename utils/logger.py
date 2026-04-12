cfd1924022d862952837f13da1a2b1b2dc6b5ca3
Cameron Tran <camessages357@gmail.com>
2026-04-12 14:52:53 -0700

    Actually updated logger.py
    Temporary, will revise if needed.

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,18 @@
 # Prints information about what the system is doing
+import datetime
+
+def _get_time():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def log(message):
+    print(f"[{_get_time()}][LOG] {message}")
+
+def info(message):
+    print(f"[{_get_time()}][INFO] {message}")
+
+def warning(message):
+    print(f"[{_get_time()}][WARNING] {message}")
+
+def error(message):
+    print(f"[{_get_time()}][ERROR] {message}")
+    
