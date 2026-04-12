75bf4f9d873591f46ff46fe445cd18950fb606ff
Cameron Tran <camessages357@gmail.com>
2026-04-12 14:51:29 -0700

    Updated main.py and logger.py
    Temporary, will add more.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Run full pipeline
+# Import necessary modules
+
+def main():
+    # Start up system
+    # Get camera frame, if it fails use dummy data
+    # Runs detection
+    # Runs decision logic
+    # Executes movement
+    # Logs everything
+    pass
+
+if __name__ == "__main__":
+    main()
